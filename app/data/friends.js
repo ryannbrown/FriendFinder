0d0e395fdd236f0faf86bc6a738f707bb69fed8f
ryannbrown <44414542+ryannbrown@users.noreply.github.com>
2019-05-11 17:38:31 -0400

    Fixed bracket in friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+var friends= [
+{
+    "name":"Daniel",
+    "photo":"https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/17309533_10154920158265926_832381316771865237_n.jpg?_nc_cat=110&_nc_ht=scontent-iad3-1.xx&oh=7a2a943bf5e4151b0f3fb0c68a6d9104&oe=5D682F18",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  }
+];
